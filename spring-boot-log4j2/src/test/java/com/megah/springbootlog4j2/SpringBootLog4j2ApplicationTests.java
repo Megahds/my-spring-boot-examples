066508da400513bfa77433880de58d5fcdd0aec9
Megahds <megahdiamondteam@gmail.com>
2022-06-20 11:30:41 +0700

    first commit

---
+++ spring-boot-log4j2/src/test/java/com/megah/springbootlog4j2/SpringBootLog4j2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megah.springbootlog4j2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLog4j2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
